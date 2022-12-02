4973867a41eadc1ebbbd2567e2fbd29a58ac42e9
choiyounghwan <egoing@gmail.com>
2022-12-03 01:24:30 +0900

    중간 확인

---
+++ src/component/TodoInsert.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const TodoInsert = () => {
+  return (
+    <form className="todoInsert">
+      <input placeholder="할일을 입력하세요."></input>
+      <button type="submit"></button>
+    </form>
+  );
+};
+
+export default TodoInsert;
